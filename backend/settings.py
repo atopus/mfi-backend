2f54be963b07146248c6ac848d1e8801d78749a8
Paul Antropius <paul.antropius@kstor.net>
2022-02-16 17:37:57 +0100

    Move secret_key to localsettings.

--- backend/settings.py
+++ backend/settings.py
@@ -15,12 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--7x5s2tf(!1fy&n(lrc)3a@4d(31qyphnk6b86=pjs83otx2xh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
